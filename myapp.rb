32d60323753a0ca54a475a4138f967cdedbee604
tomnatt <github@tomnatt.com>
2013-11-16 14:31:32 +0000

    stage 1 of handling checkbox input

--- myapp.rb
+++ myapp.rb
@@ -14,9 +14,9 @@ class MyApp < Sinatra::Base
     users = Hash.new
     users["green"] = SteamUser.new("Green", 76561198000976107)
     users["laggy"] = SteamUser.new("Laggy", 76561197970651383)
-    users["wheels"] = SteamUser.new("Wheels", 76561198001124293)
-    users["twosid"] = SteamUser.new("Twosid", 76561197977955065)
-    users["jimmy"] = SteamUser.new("Jimmy", 76561197999187925)
+    #users["wheels"] = SteamUser.new("Wheels", 76561198001124293)
+    #users["twosid"] = SteamUser.new("Twosid", 76561197977955065)
+    #users["jimmy"] = SteamUser.new("Jimmy", 76561197999187925)
 
     # load the appid to name data
     url = "http://api.steampowered.com/ISteamApps/GetAppList/v0001/"
@@ -35,11 +35,19 @@ class MyApp < Sinatra::Base
 
     get '/compare' do
 
-        u = params["u"].split(",")
+        selected = Array.new
+        params.each do |key, value|
+            # use the param if it's user0, etc
+            if key =~ /user\d+/ then
+                selected << value
+            end
+        end
+        puts selected
+
 
         # get the intersection of the games list and convert to proper names
         # todo make this more intelligent, and power from the parameter
-        sharedGamesIds = (users["green"].games & users["laggy"].games) & users["twosid"].games & users["jimmy"].games 
+        sharedGamesIds = (users["green"].games & users["laggy"].games)
         sharedGames = Array.new
         sharedGamesIds.each do |g|
             sharedGames << appHash[g]
